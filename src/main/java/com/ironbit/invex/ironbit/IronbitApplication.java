e69dad5d2d96a83804050b31f29fc591bfd3e489
David Loyo <david@loyo.dev>
2023-10-16 08:28:19 -0600

    init

---
+++ src/main/java/com/ironbit/invex/ironbit/IronbitApplication.java
@@ -0,0 +1,13 @@
+package com.ironbit.invex.ironbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironbit/invex/ironbit/IronbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironbit.invex.ironbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
